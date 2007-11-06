6dbb64d5d0e477cf6e43735620b3e5987d7c86d5
KevinKaoAtSun <KevinKaoAtSun@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2007-11-06 17:28:50 +0000

    Get target query builder name from master.xml

--- index-gui/src/main/java/com/sun/mdm/index/project/anttasks/LoaderGeneratorTask.java
+++ index-gui/src/main/java/com/sun/mdm/index/project/anttasks/LoaderGeneratorTask.java
@@ -27,102 +27,92 @@ import com.sun.mdm.index.project.EviewRepository;
  * 
  */
 public class LoaderGeneratorTask extends Task {
-	private String srcDir;
-	private String configDir;
+    private String srcDir;
+    private String configDir;
 
-	public void setSrcDir(String srcDir) {
-		this.srcDir = getProjectPath() + File.separator + srcDir;
-	}
+    public void setSrcDir(String srcDir) {
+        this.srcDir = getProjectPath() + File.separator + srcDir;
+    }
 
-	public void setConfigDir(String configDir) {
-		this.configDir = getProjectPath() + File.separator + configDir;
-	}
+    public void setConfigDir(String configDir) {
+        this.configDir = getProjectPath() + File.separator + configDir;
+    }
 
-	/**
-	 * @return
-	 */
-	private String getProjectPath() {
-		return getProject().getProperty("basedir");
-	}
+    /**
+     * @return
+     */
+    private String getProjectPath() {
+        return getProject().getProperty("basedir");
+    }
 
-	@Override
-	public void execute() throws BuildException {
-		LoaderConfig config = new LoaderConfig();
-		write(config);//TODO delete this 
-		try {
-			File xmlFile;
-			InputSource source;
-			String str;
+    @Override
+    public void execute() throws BuildException {
+        LoaderConfig config = new LoaderConfig();
+        try {
+            File xmlFile;
+            InputSource source;
+            String str;
+           	
+            // Threshold
+            xmlFile = new File(srcDir + File.separator
+		+ EviewRepository.CONFIGURATION_FOLDER + File.separator	+ "master.xml");
+            source = new InputSource(new FileInputStream(xmlFile));
+            MasterType masterType = Utils.parseMasterType(source);
+            com.sun.mdm.index.parser.Utils.Parameter parameterDuplicateThreshold;
+            com.sun.mdm.index.parser.Utils.Parameter parameterMatchThreshold;
+            parameterDuplicateThreshold = masterType.getDecisionMakerConfigParameterByName(MasterType.DUPLICATETHRESHOLD);
+            String duplicateThreshold = "7.25";
+            String matchThreshold = "29.0";
+            if (parameterDuplicateThreshold != null) {
+		duplicateThreshold = parameterDuplicateThreshold.getValue();
+            }
+            parameterMatchThreshold = masterType.getDecisionMakerConfigParameterByName(MasterType.MATCHTHRESHOLD);
+            if (parameterMatchThreshold != null) {
+		matchThreshold = parameterMatchThreshold.getValue();
+            }
+            String strThreshold = "<threshold-config>\n"
+				+ "    <duplicateThreshold>" + duplicateThreshold
+				+ "</duplicateThreshold>\n" + "    <matchThreshold>"
+				+ matchThreshold + "</matchThreshold>\n"
+				+ "</threshold-config>\n";
 
-			xmlFile = new File(srcDir + File.separator
-					+ EviewRepository.CONFIGURATION_FOLDER + File.separator
-					+ "master.xml");
-			source = new InputSource(new FileInputStream(xmlFile));
-			MasterType masterType = Utils.parseMasterType(source);
-			com.sun.mdm.index.parser.Utils.Parameter parameterDuplicateThreshold;
-			com.sun.mdm.index.parser.Utils.Parameter parameterMatchThreshold;
-			parameterDuplicateThreshold = masterType
-					.getDecisionMakerConfigParameterByName(MasterType.DUPLICATETHRESHOLD);
-			String duplicateThreshold = "7.25";
-			String matchThreshold = "29.0";
-			if (parameterDuplicateThreshold != null) {
-				duplicateThreshold = parameterDuplicateThreshold.getValue();
-			}
-			parameterMatchThreshold = masterType
-					.getDecisionMakerConfigParameterByName(MasterType.MATCHTHRESHOLD);
-			if (parameterMatchThreshold != null) {
-				matchThreshold = parameterMatchThreshold.getValue();
-			}
-			String strThreshold = "<threshold-config>\n"
-					+ "    <duplicateThreshold>" + duplicateThreshold
-					+ "</duplicateThreshold>\n" + "    <matchThreshold>"
-					+ matchThreshold + "</matchThreshold>\n"
-					+ "</threshold-config>\n";
+            config.setThreshold(strThreshold);
+            
+            // QueryBuilder
+            xmlFile = new File(srcDir + File.separator + 
+                EviewRepository.CONFIGURATION_FOLDER + File.separator +"query.xml");
+            source = new InputSource(new FileInputStream(xmlFile));
+            QueryType queryType = Utils.parseQueryType(source);
+            String queryBuilderName = masterType.getExecuteMatchQueryBuilderName();
+            QueryBuilder queryBuilder = queryType.getQueryBuilderByName(queryBuilderName);
+            str = queryType.getQueryBuilderXML(queryBuilder);
+            
+            config.setBlockConfig(str);
+            
+            // MatchConfig
+            xmlFile = new File(srcDir + File.separator +
+                EviewRepository.CONFIGURATION_FOLDER + File.separator +"mefa.xml");
+            source = new InputSource(new FileInputStream(xmlFile));
+            MatchFieldDef matchFieldDef = Utils.parseMatchFieldDef(source);
+            str = matchFieldDef.getMatchingConfigXML();
+            
+            config.setMatchConfig(str);
 
-			// config.setThreshold("");
+            // Write it out
+            write(config);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-			// QueryBuilder
-			xmlFile = new File(srcDir + File.separator
-					+ EviewRepository.CONFIGURATION_FOLDER + File.separator
-					+ "query.xml");
-			source = new InputSource(new FileInputStream(xmlFile));
-			QueryType queryType = Utils.parseQueryType(source);
-			QueryBuilder queryBuilder = queryType
-					.getQueryBuilderByName("BLOCKER_SEARCH");
-			str = queryType.getQueryBuilderXML(queryBuilder);
-
-			config.setBlockConfig(str);
-
-			// MatchConfig
-			xmlFile = new File(srcDir + File.separator
-					+ EviewRepository.CONFIGURATION_FOLDER + File.separator
-					+ "mefa.xml");
-			source = new InputSource(new FileInputStream(xmlFile));
-			MatchFieldDef matchFieldDef = Utils.parseMatchFieldDef(source);
-			str = matchFieldDef.getMatchingConfigXML();
-
-			config.setMatchConfig(str);
-
-			write(config);
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	private void write(LoaderConfig config) {
-		try {
-			File f = new File(configDir + "/loader-config.xml");
-
-			BufferedWriter wr = new BufferedWriter(new FileWriter(f));
-
-			wr.write(config.toString());
-
-			wr.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
+    private void write(LoaderConfig config) {
+        try {
+            File f = new File(configDir + "/loader-config.xml");
+            BufferedWriter wr = new BufferedWriter(new FileWriter(f));
+            wr.write(config.toString());
+            wr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
