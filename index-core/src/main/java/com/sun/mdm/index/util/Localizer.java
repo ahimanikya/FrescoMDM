576626020cb2522eef52e4afef37ba55988edbfc
cubiclecowboy <cubiclecowboy@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2007-12-11 19:32:16 +0000

    Changed i18n identifier from "MDM-MI" to "DM-MI".

--- index-core/src/main/java/com/sun/mdm/index/util/Localizer.java
+++ index-core/src/main/java/com/sun/mdm/index/util/Localizer.java
@@ -32,7 +32,7 @@ import net.java.hulp.i18n.LocalizationSupport;
 public class Localizer extends LocalizationSupport {
     
     private static final String DEFAULT_PATTERN = "([A-Z][A-Z][A-Z]\\d\\d\\d)(:)(.*)";
-    private static final String DEFAULT_PREFIX = "MDM-MI-";
+    private static final String DEFAULT_PREFIX = "DM-MI-";
     private static final String DEFAULT_BUNDLENAME = "msgs";
     private static Localizer instance = null; 
     
