7908a53796b2044ec426a190d534d155a4eb86f4
swaranjitdua <swaranjitdua@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-01-23 03:23:54 +0000

    interfaces to new MatchEngine

---
+++ loader/src/com/sun/mdm/index/loader/matcher/MatcherAdapter.java
@@ -0,0 +1,110 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2003-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the terms of the Common 
+ * Development and Distribution License ("CDDL")(the "License"). You 
+ * may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://open-dm-mi.dev.java.net/cddl.html
+ * or open-dm-mi/bootstrap/legal/license.txt. See the License for the 
+ * specific language governing permissions and limitations under the  
+ * License.  
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice 
+ * in each file and include the License file at
+ * open-dm-mi/bootstrap/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the 
+ * fields enclosed by brackets [] replaced by your own identifying 
+ * information: "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package com.sun.mdm.index.loader.matcher;
+import java.io.InputStream;
+import java.util.HashSet;
+
+import com.sun.mdm.matcher.api.MatchConfigFilesAccess;
+import com.sun.mdm.matcher.api.ConfigFilesAccess;
+import com.sun.mdm.matcher.api.MatcherConfigurationException;
+import com.sun.mdm.matcher.api.MatchingEngine;
+//import com.stc.sbmeapi.impl.StandConfigFilesAccessImpl;
+import com.sun.mdm.matcher.api.impl.MatchConfigFilesAccessImpl;
+
+
+public class MatcherAdapter {
+    
+    private  MatchingEngine mSbmeMatch;
+    private  String[] matchFieldIDs;
+
+    public MatcherAdapter(String[] matchFldIds) throws Exception {
+     
+    	 matchFieldIDs = matchFldIds;	
+        //ConfigFilesAccess files = new NewMatcher.CSConfigFileAccess();
+        String path = "match/";        
+        MatchConfigFilesAccess files = new MatchConfigFilesAccessImpl(path); 
+        mSbmeMatch = new MatchingEngine(files);
+//        mSbmeMatch.initializeData(files);
+        mSbmeMatch.upLoadConfigFile();
+//        StandConfigFilesAccessImpl filesAccess = new StandConfigFilesAccessImpl();
+
+ 
+    }
+        
+      
+    
+    public float compareRecords(String[] rec1, String[] rec2) throws Exception {
+        
+        String [][] candRecArrayVals = { (rec1) };
+        String [][] refRecArrayVals = { (rec2) };
+        
+        double[][] score = mSbmeMatch.matchWeight(matchFieldIDs, candRecArrayVals, refRecArrayVals);
+        return (float) score[0][0];
+    }
+    
+    public float compareRecords(String rec1, String rec2, String matchField) throws Exception {
+        
+        String [][] candRecArrayVals = { {rec1} };
+        String [][] refRecArrayVals = { {rec2} };
+        String [] matchFields = {matchField};
+    	//mSbmeMatch.m
+        
+        double[][] score = mSbmeMatch.matchWeight(matchFields, candRecArrayVals, refRecArrayVals);
+        return (float) score[0][0];
+    }
+    
+    public static class CSConfigFileAccess implements ConfigFilesAccess {
+        
+        public java.io.InputStream getConfigFileAsStream(String name)
+        throws MatcherConfigurationException {
+            
+            InputStream stream = null;
+            
+            try {
+                stream = ClassLoader.getSystemResourceAsStream("match/" + name);
+            	//stream = ClassLoader.getSystemResourceAsStream(name);
+            } catch (RuntimeException ex) {
+                System.out.println("Error: " + ex);
+                throw new MatcherConfigurationException(ex);
+            }
+            
+            return stream;
+        }
+        
+        public java.io.InputStream getConfigFileAsStream(String name, String name2)
+        throws MatcherConfigurationException {
+            
+            InputStream stream = null;
+            
+            try {
+                stream = ClassLoader.getSystemResourceAsStream("match/" + name);
+            } catch (RuntimeException ex) {
+                System.out.println("Error: " + ex);
+                throw new MatcherConfigurationException(ex);
+            }
+            
+            return stream;
+        }
+    }
+    
+}
