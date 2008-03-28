0cd5bed315f0d686c279226bbcf2fa0d048df13f
khasling <khasling@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-03-28 23:49:32 +0000

    Updating to SM09

--- tl/src/cmn/codeline/codeline.pl
+++ tl/src/cmn/codeline/codeline.pl
@@ -62,7 +62,7 @@ $PRODUCT = "odmmi";
 
 #this is the current release number:
 %MILESTONE_VERSION = (
-    'odmmi', 'SM08',
+    'odmmi', 'SM09',
 );
 
 #this is the milestone date this release is based on.
