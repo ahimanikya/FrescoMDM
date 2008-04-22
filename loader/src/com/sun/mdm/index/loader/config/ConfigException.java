f3a9f90396e4c244467df6147f1ec7ff66de5d00
cye <cye@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-04-22 04:10:32 +0000

    added loader config validation and tunning

---
+++ loader/src/com/sun/mdm/index/loader/config/ConfigException.java
@@ -0,0 +1,54 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2003-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the terms of the Common 
+ * Development and Distribution License ("CDDL")(the "License"). You 
+ * may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://open-dm-mi.dev.java.net/cddl.html
+ * or open-dm-mi/bootstrap/legal/license.txt. See the License for the 
+ * specific language governing permissions and limitations under the  
+ * License.  
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice 
+ * in each file and include the License file at
+ * open-dm-mi/bootstrap/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the 
+ * fields enclosed by brackets [] replaced by your own identifying 
+ * information: "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package com.sun.mdm.index.loader.config;
+
+/** 
+ * This exception class represents Configuration errors. 
+ * 
+ * @author cye
+ */
+public class ConfigException extends Exception {
+
+     
+	/**
+	 * @param cause the cause.
+	 */
+    public ConfigException(Throwable cause) {
+        super(cause);
+    }
+        
+    /** 
+     * @param the exception message.
+     */
+    public ConfigException(String message) {
+    	super(message);
+    }
+ 
+    /**
+     * @param message the exception message.
+     * @param cause the cause. 
+     */
+    public ConfigException(String message, Throwable cause) {
+    	super(message + "\ncause: " + (cause==null ? "not known" : cause.getMessage()), cause);
+    }
+}
