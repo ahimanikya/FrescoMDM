21451c3f20c97ecc4ea75ddd7f4fc64e43c9992f
swaranjitdua <swaranjitdua@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-05-05 21:39:31 +0000

    code cleanup

--- loader/src/com/sun/mdm/index/loader/blocker/BlockDefinition.java
+++ loader/src/com/sun/mdm/index/loader/blocker/BlockDefinition.java
@@ -41,26 +41,23 @@ import com.sun.mdm.index.filter.FilterConstants;
  * BlockDefinition
  * Represents Master Index block definition.
  * 
- * Most of the apis are used to create and setup BLockDefinition and called  
- * based upon Block Defintion configuration file.
+ * Two kind of APIs: 
+ *   1. Block Definiiton setup APIs. Used during configuration of BlockDefinition
+ *   2. Run time APIs - getBlockIDs() create and return blockids based on input Dataobject.
  * 
- * After BlockDefintion has been initialized in the loader, 
- * getBlockIDs() is called by BlockDistributor to get BlockID for each
- * input DataObject.
- * 
- * BlockDefintion contains Rule class that can be set up as either
+ * BlockDefintion is container of Rule class that can be set up as either
  * in OR or AND state.
  * 
- * AND for two field values leads to matrix values muliplication.
+ * AND for two field values leads to matrix values multiplication.
 		      Say field1 values= {"1", "2", "3"} 
-		    *  field2 values = {"a", "b"}
-		    *  Then "AND" operator would give:
-		    *  {"1a", "1b","2a", "2b", "3a", "3b"}
-		      
+ *  field2 values = {"a", "b"}
+ *  Then "AND" operator would give:
+ *  {"1a", "1b","2a", "2b", "3a", "3b"}
+
   "OR" leads to addition of field values.
           So it would result in :
 		      {"1", "2", "3", "a", "b"}
-		   
+
  * @author Swaranjit Dua
  *
  */
@@ -71,32 +68,37 @@ public class BlockDefinition {
 	private List<Rule> rules_ = new ArrayList<Rule>();
 	public enum Operator {AND, OR};
 	private Operator operator_ = Operator.AND;
-	
+
 	/*
-	 * configuration api
+	 * configuration api. Set Block Definition ID
 	 */
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	/*
-	 * configuration api
+	 * configuration api. Add Rule
 	 */
 	public void addRule(Rule rule) {
-    	rules_.add(rule);
-    }
-	/*
-	 * configuration api
+		rules_.add(rule);
+	}
+	/**
+	 * configuration api. Add rule
+	 * @param epath Epath string
 	 */
 	public void addRule(String epath) throws Exception {
-    	Rule rule = new Rule(epath);
-    	rules_.add(rule);
-    }
-	
+		Rule rule = new Rule(epath);
+		rules_.add(rule);
+	}
+
+	/**
+	 * configuration api. Add rule
+	 * @param epath Epath string. Source is from BlockDefinition source
+	 */
 	public void addRule(String epath,String source) throws Exception {
-    	Rule rule = new Rule(epath,source);
-    	rules_.add(rule);
-    }
+		Rule rule = new Rule(epath,source);
+		rules_.add(rule);
+	}
 	/**
 	 * merges List of block defintions into one using passed operator operator.
 	 * @param bdef BlockDefinitions to merge
@@ -104,9 +106,9 @@ public class BlockDefinition {
 	 * @return merged BlockDefinition
 	 */
 	public static BlockDefinition merge(List<BlockDefinition> bdefs,
-			                        Operator operator) {
+			Operator operator) {
 		BlockDefinition targetdef = new BlockDefinition();
-				
+
 		for (BlockDefinition def: bdefs) {
 			Rule rule = new Rule();
 			rule.children = def.rules_;
@@ -118,25 +120,28 @@ public class BlockDefinition {
 		}
 		targetdef.operator_ = operator;		
 		return targetdef;
-		
+
 	}
-	
-	
-	 /*
-		 * Invoked by BlockDistributor to get BlockIDs for each dataObject.
-		 */
-	 
+
+
+	/*
+	 * 
+	 * Invoked by BlockDistributor to get BlockIDs for each dataObject.
+	 *  @param DataObject dataobject 
+	 *  @param lk Lookup used for getting desired values from dataobject
+	 */
+
 	List<String> getBlockIds(DataObject dataObject, Lookup lk) throws Exception {
-	   List<String> blockids = new ArrayList<String>();
-	   for (int i = 0; i < rules_.size(); i++) {
-		   List<String> values = rules_.get(i).getValue(dataObject, lk);
-		   if (values != null) {
-		     blockids = add(blockids, values, operator_);
-		   }
-	   }
-	   return insertIds(blockids);	   
+		List<String> blockids = new ArrayList<String>();
+		for (int i = 0; i < rules_.size(); i++) {
+			List<String> values = rules_.get(i).getValue(dataObject, lk);
+			if (values != null) {
+				blockids = add(blockids, values, operator_);
+			}
+		}
+		return insertIds(blockids);	   
 	}
-	
+
 	public String toString() {
 		StringBuffer buf = new StringBuffer("Block Definition:");
 		for (int i = 0; i < rules_.size(); i++) {
@@ -146,234 +151,229 @@ public class BlockDefinition {
 		}
 		return buf.toString();
 	}
-	
-	
+
+	/**
+	 * This represents Master Index BlockDefinition Rule
+	 * @author Swaranjit Dua
+	 *
+	 */
 	public static class Rule {
 		private List<Rule> children = new ArrayList<Rule>();
-		
+
 		private boolean isLeaf = false;
 		private EPath epath;
 		private String qname;
 		private String source;
 		private Operator operator_ = Operator.AND;  // AND default operator
-	    private BlockIDGenerator blockIDGenerator = null;
-	   
-	    public Rule() {
-	    	
-	    }
-	    public Rule(String epath) throws Exception {
-	    	isLeaf = true;
-	    	parseEpath(epath);
-	    }
-	    
-	    public Rule(String epath,String source) throws Exception {
-	    	isLeaf = true;
-	    	parseEpath(epath);
-	    	this.source=source;
-	    }
+		private BlockIDGenerator blockIDGenerator = null;
 
-	    
-	    public Rule(List<String> epaths) throws Exception {
-	    	for (int i = 0; i < epaths.size(); i++) {
-	    	  children.add(new Rule(epaths.get(i)));
-	    	}
-	    	//operator = Operator.AND;
-	    }
-	    
-	    public Rule(String[] epaths) throws Exception  {
-	    	for (int i = 0; i < epaths.length; i++) {
-	    	  children.add(new Rule(epaths[i]));
-	    	}
-	    	//operator = Operator.AND;
-	    }
-	    
-	    /*
-		 * configuration api
-		 */
-	    public void addChild(Rule rule) {
-	    	children.add(rule);
-	    }
-	    /*
-		 * configuration api
-		 */
-	    public void setLeaf(boolean leaf) {
-	       isLeaf = leaf;	
-	    }
-	    /*
-		 * configuration api
-		 */
-	    public void setOperator(Operator oper) {
-	    	operator_ = oper;
-	    }
-	  /*  
-	    void setEpath(String epath) throws Exception {
-	    	isLeaf = true;
-	    	parseEpath(epath);
-	    	
-	    }
-	    */
-	    
-	    
-		private List<String> getValue(DataObject dataObject, Lookup lk) throws Exception {
-			List<String> allvalues = new ArrayList<String>();
-			if (blockIDGenerator != null) {
-				allvalues = blockIDGenerator.computeBlockID(epath, dataObject, lk);
+		public Rule() {
+
+		}
+		public Rule(String epath) throws Exception {
+			isLeaf = true;
+			parseEpath(epath);
+		}
+
+		public Rule(String epath,String source) throws Exception {
+			isLeaf = true;
+			parseEpath(epath);
+			this.source=source;
+		}
+
+
+		public Rule(List<String> epaths) throws Exception {
+			for (int i = 0; i < epaths.size(); i++) {
+				children.add(new Rule(epaths.get(i)));
 			}
-			else if (isLeaf) {								
+
+		}
+
+		public Rule(String[] epaths) throws Exception  {
+			for (int i = 0; i < epaths.length; i++) {
+				children.add(new Rule(epaths[i]));
+			}
+
+		}
+
+		/*
+		 * configuration api
+		 */
+		 public void addChild(Rule rule) {
+			children.add(rule);
+		}
+		 /*
+		  * configuration api
+		  */
+		 public void setLeaf(boolean leaf) {
+			 isLeaf = leaf;	
+		 }
+		 /*
+		  * configuration api
+		  */
+		 public void setOperator(Operator oper) {
+			 operator_ = oper;
+		 }
+
+
+		 private List<String> getValue(DataObject dataObject, Lookup lk) throws Exception {
+			 List<String> allvalues = new ArrayList<String>();
+			 if (blockIDGenerator != null) {
+				 allvalues = blockIDGenerator.computeBlockID(epath, dataObject, lk);
+			 }
+			 else if (isLeaf) {								
 				 try {
-					List<String> values = DOEpath.getFieldValueList(epath, dataObject, lk);
-					values = filter(qname, values);			
-					if (values != null ) {
-					  allvalues.addAll(values);
-					}
+					 List<String> values = DOEpath.getFieldValueList(epath, dataObject, lk);
+					 values = filter(qname, values);			
+					 if (values != null ) {
+						 allvalues.addAll(values);
+					 }
 				 }  catch (EPathException ex)  {
 					 logger.info(ex.getMessage());
 					 throw ex;					 
 				 }
 
-			} else {
-				for (int i = 0; i < children.size(); i++) {
-					Rule rule = children.get(i);
-					List<String> values = rule.getValue(dataObject, lk);
-					if (values == null || values.size()==0  ) {
-					  if (operator_.equals(Operator.AND)) {
-						  return null;
-					  }
-					}
-					
-					allvalues = add(allvalues, values, operator_);
-				}				
-			}
-			return (allvalues.size()==0 ? null: allvalues);
-		}
-		
-		private List<String> filter(String epath, List<String> values) {
-			if (values == null) {
-				return null;
-			}
-			List<String> newvalues = new ArrayList<String>();
-			for (int i = 0; i < values.size(); i++) {
-				String  value = values.get(i);
-				if (!isFieldValueFiltered(epath, value)) {
-				     newvalues.add(value);
-					//values.set(i, null);
-				
-				}
-			}
-			if (newvalues.size() == 0) { // if empty, return null
-				return null;
-			}
-			return newvalues;
-		}
-		
-		private boolean isFieldValueFiltered(String field, String value) {
-			if (value == null || value.equals("")) {
-				return true;
-			}
-			ExclusionListLookup exlookup = new ExclusionListLookup();
-			return exlookup.isFieldValueInExclusion(value, field, FilterConstants.BLOCK_EXCLUSION_TYPE);			
-			/*
-			if (field.equals("Person.FirstName")) {
-				
-				if ( value != null && value.equals("John")) {
-				  return true;
-				}
-			}
-			*/
-			//return false;
-		}
-		
-		private void parseEpath(String path) throws Exception {
-			String e = path;
-			int index = path.indexOf('+');
-			if (index > 0) {
-			  e = path.substring(0, index);
-			  if (index + 1 < path.length()) {
-			    String blockIDGclass = path.substring(index+1);
-			    
-			    Class c = Class.forName(blockIDGclass);
-			    blockIDGenerator = (BlockIDGenerator)c.newInstance();
-			  }
-			}
-			qname = e;
-			e = convertStarEpath(e);
-			this.epath = EPathParser.parse(e);
-			
-		}
-		
-		public String toString() {
-			StringBuffer buf = new StringBuffer("Rule:");
-		   if (isLeaf) {
-			   buf.append( "field:" + epath.toString());
-		   } else {
-			   
-			   buf.append("Operator:" + operator_);
-				for (int i = 0; i < children.size(); i++) {
-						Rule rule = children.get(i);
-						String str = rule.toString();
-						buf.append(str);
-						
-				}
-		   }
-		   return buf.toString();
-		}
-		public String getSource() {
-			return source;
-		}
-		public boolean isLeaf() {
-			return isLeaf;
-		}
-		public List<Rule> getChildren() {
-			return children;
-		}			
-					
-  }
-	
+			 } else {
+				 for (int i = 0; i < children.size(); i++) {
+					 Rule rule = children.get(i);
+					 List<String> values = rule.getValue(dataObject, lk);
+					 if (values == null || values.size()==0  ) {
+						 if (operator_.equals(Operator.AND)) {
+							 return null;
+						 }
+					 }
+
+					 allvalues = add(allvalues, values, operator_);
+				 }				
+			 }
+			 return (allvalues.size()==0 ? null: allvalues);
+		 }
+
+		 private List<String> filter(String epath, List<String> values) {
+			 if (values == null) {
+				 return null;
+			 }
+			 List<String> newvalues = new ArrayList<String>();
+			 for (int i = 0; i < values.size(); i++) {
+				 String  value = values.get(i);
+				 if (!isFieldValueFiltered(epath, value)) {
+					 newvalues.add(value);
+
+
+				 }
+			 }
+			 if (newvalues.size() == 0) { // if empty, return null
+				 return null;
+			 }
+			 return newvalues;
+		 }
+
+		 private boolean isFieldValueFiltered(String field, String value) {
+			 if (value == null || value.equals("")) {
+				 return true;
+			 }
+			 ExclusionListLookup exlookup = new ExclusionListLookup();
+			 return exlookup.isFieldValueInExclusion(value, field, FilterConstants.BLOCK_EXCLUSION_TYPE);			
+
+		 }
+
+		 private void parseEpath(String path) throws Exception {
+			 String e = path;
+			 int index = path.indexOf('+');
+			 if (index > 0) {
+				 e = path.substring(0, index);
+				 if (index + 1 < path.length()) {
+					 String blockIDGclass = path.substring(index+1);
+
+					 Class c = Class.forName(blockIDGclass);
+					 blockIDGenerator = (BlockIDGenerator)c.newInstance();
+				 }
+			 }
+			 qname = e;
+			 e = convertStarEpath(e);
+			 this.epath = EPathParser.parse(e);
+
+		 }
+
+		 public String toString() {
+			 StringBuffer buf = new StringBuffer("Rule:");
+			 if (isLeaf) {
+				 buf.append( "field:" + epath.toString());
+			 } else {
+
+				 buf.append("Operator:" + operator_);
+				 for (int i = 0; i < children.size(); i++) {
+					 Rule rule = children.get(i);
+					 String str = rule.toString();
+					 buf.append(str);
+
+				 }
+			 }
+			 return buf.toString();
+		 }
+		 public String getSource() {
+			 return source;
+		 }
+		 public boolean isLeaf() {
+			 return isLeaf;
+		 }
+		 public List<Rule> getChildren() {
+			 return children;
+		 }			
+
+	}
+	/**
+	 * adds source values to values, based on operator AND/OR.
+	 * @param values
+	 * @param source
+	 * @param operator
+	 * @return
+	 */
 	private static List<String> add(List<String> values, List<String> source, Operator operator) {
-	    List<String> retvalues = new ArrayList<String>();
-	    if (values.size() == 0) {
-			   retvalues = source;
+		List<String> retvalues = new ArrayList<String>();
+		if (values.size() == 0) {
+			retvalues = source;
 		} else if (source.size() == 0) {
-			   retvalues = values;
+			retvalues = values;
 		} else if (operator.equals(Operator.AND)) {
-			
-		   /*
-		    *  AND leads to matrix values muliplication.
-		    *  So say values= {"1", "2", "3"}
-		    *  source = {"a", "b"}
-		    *  Then "AND" operator would result in:
-		    *  {"1a", "1b","2a", "2b", "3a", "3b"}
-		    *  
-		    *  "OR" would result in :
-		    *  {"1", "2", "3", "a", "b"}
-		    *  
-		    */
-		   	if (values == null || values.size() == 0 || source == null || source.size() == 0) {
-		   		return null;
-		   	}
-		   	
-		    for (int i = 0; i < values.size(); i++ ) {
-			   for (int j = 0; j < source.size(); j++) {
-				   String value = values.get(i) + source.get(j);
-				   retvalues.add(value);
-			   }
-		     }
-		   
-	    } else {
-		     /*
-		      * OR so do just addition
-		      */
-		       retvalues = values;
-		   
-		      for (int j = 0; j < source.size(); j++) {
-			    String value = source.get(j);
-		        retvalues.add(value);
-		      }
-		   
-	    }
+
+			/*
+			 *  AND leads to matrix values muliplication.
+			 *  So say values= {"1", "2", "3"}
+			 *  source = {"a", "b"}
+			 *  Then "AND" operator would result in:
+			 *  {"1a", "1b","2a", "2b", "3a", "3b"}
+			 *  
+			 *  "OR" would result in :
+			 *  {"1", "2", "3", "a", "b"}
+			 *  
+			 */
+			if (values == null || values.size() == 0 || source == null || source.size() == 0) {
+				return null;
+			}
+
+			for (int i = 0; i < values.size(); i++ ) {
+				for (int j = 0; j < source.size(); j++) {
+					String value = values.get(i) + source.get(j);
+					retvalues.add(value);
+				}
+			}
+
+		} else {
+			/*
+			 * OR so do just addition
+			 */
+			retvalues = values;
+
+			for (int j = 0; j < source.size(); j++) {
+				String value = source.get(j);
+				retvalues.add(value);
+			}
+
+		}
 		return retvalues;
 	}
-	
+
 	private static boolean isChildEpath(EPath epath) {
 		String objectPath = epath.getLastChildPath();
 		String objectType = epath.getLastChildName();
@@ -382,7 +382,7 @@ public class BlockDefinition {
 		}
 		return true;
 	}
-   
+
 	public static void main(String[] args) {
 		try {
 			BlockDefinition[] blockdefs = new BlockDefinition[3];
@@ -395,42 +395,51 @@ public class BlockDefinition {
 			ruleC.setOperator(Operator.OR);
 			blockdefs[0].addRule(ruleC);
 			blockdefs[0].setId("1");
-			
+
 			blockdefs[1] = new BlockDefinition();
 			blockdefs[1].addRule("Person.SSN");
 			blockdefs[1].setId("2");
-			
+
 			blockdefs[2] = new BlockDefinition();
 			blockdefs[2].addRule("Person.FnamePhoneticCode");
 			blockdefs[2].addRule("Person.Gender");
 			blockdefs[2].addRule("Person.DOB");
 			blockdefs[2].setId("3");
-	
+
 			String str = null;
 			for (int i =0; i < blockdefs.length; i++) {
-			   str = blockdefs[i].toString();	
+				str = blockdefs[i].toString();	
 			}			
 		} catch (Exception ex) {
-		    System.out.println(ex);	
+			System.out.println(ex);	
 		}	
 	}
-	
+	/**
+	 * Prefix BlockDefinition ids to Blockid value.
+	 * @param blockids
+	 * @return
+	 */
 	private List<String> insertIds(List<String> blockids) {
-	  List<String> blkids = new ArrayList<String>();
-	  blockids = removeDups(blockids);
-	  if (blockids == null || blockids.size() == 0) {
-		  return null;
-	  }
-	  for (int i = 0; i < blockids.size(); i++) {  
-		  
-		  blkids.add(id + ":" + blockids.get(i));
-	  }
-	  if (blkids.contains("null")) {
-		logger.info("blockids");  
-	  }
-	  return blkids;
+		List<String> blkids = new ArrayList<String>();
+		blockids = removeDups(blockids);
+		if (blockids == null || blockids.size() == 0) {
+			return null;
+		}
+		for (int i = 0; i < blockids.size(); i++) {  
+
+			blkids.add(id + ":" + blockids.get(i));
+		}
+		if (blkids.contains("null")) {
+			//logger.info("blockids");  
+		}
+		return blkids;
 	}
-	
+
+	/**
+	 * remove any duplicate blockids from list of blockids.
+	 * @param blockids
+	 * @return
+	 */
 	private List<String> removeDups(List<String> blockids) {
 		if (blockids == null) {
 			return blockids;
@@ -445,24 +454,31 @@ public class BlockDefinition {
 				continue;
 			}
 			if (map.get(value)== null) {
-			   map.put(value, value);		
+				map.put(value, value);		
 			}
 		}
-		
+
 		Collection<String> col = map.values();
 		List<String> ret = new ArrayList<String>(col);
 		return ret;
-		
+
 	}
-		
+
+	/**
+	 * converts child "." field notation to [*] because EpathAPI recognizes [*] notation to get field values.
+	 * Block definition uses QFieldName like Person.Address.City that needs to be converted to Person.Address[*].*
+	 * to EpathAPI.
+	 * @param e
+	 * @return
+	 */
 	private static String convertStarEpath(String e) {
-	  String s = e;
-	  int index = e.indexOf(".");
-	  String firstS = s.substring(0, index);
-	  String sub = e.substring(index+1);
-	  
-	  sub =  sub.replaceAll("\\.", "[*].");
-	  return firstS + "." + sub;
+		String s = e;
+		int index = e.indexOf(".");
+		String firstS = s.substring(0, index);
+		String sub = e.substring(index+1);
+
+		sub =  sub.replaceAll("\\.", "[*].");
+		return firstS + "." + sub;
 	}
 
 	public List<Rule> getRules() {
