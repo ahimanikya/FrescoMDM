5ea16915bb8ef7ec6325d60c3c8cc7730ea52c42
swaranjitdua <swaranjitdua@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-05-05 21:41:14 +0000

    code cleanup

--- loader/src/com/sun/mdm/index/loader/main/BulkMatcherLoader.java
+++ loader/src/com/sun/mdm/index/loader/main/BulkMatcherLoader.java
@@ -46,14 +46,14 @@ import com.sun.mdm.index.loader.sqlloader.BulkLoader;
 import com.sun.mdm.index.configurator.ConfigurationService;
 
 
-/** Main class to bulk Match and load data to eview master index. 
+/** Main driver class that bulk Match  and load Master Data to master index schema. 
  *  This in turn invokes:
  *  BlockDistributor: to distribute blocks to Bucket file.
- *  Matcher: to do match on records within a block
- *  EUID Assigner: To assings EUID to the system objects.
+ *  Matcher: to do match operation on records within a block
+ *  EUID Assigner: To assign EUID to the system objects.
  *  Master Index Generator: To calculate SBR, transactions, assumed matches,
  *  Enterprise Objects.
- *  Database Loader: to load data into eview master index
+ *  Database Loader: to load data into master index
  *  Potential duplicate Generator: To generate potential duplicates
  * 
  * @author Swaranjit Dua
@@ -61,7 +61,7 @@ import com.sun.mdm.index.configurator.ConfigurationService;
  */
 
 public class BulkMatcherLoader {
-	
+
 	private static Logger logger = Logger.getLogger(BulkMatcherLoader.class
 			.getName());
 	private LoaderConfig config_ = LoaderConfig.getInstance();
@@ -85,141 +85,141 @@ public class BulkMatcherLoader {
 	private int loadMode_ = 0; // default mode
 	private ObjectDefinition inputobd_;
 	private String dateFormatString_;
-	
+
 	public BulkMatcherLoader() throws Exception {
 		RuntimeStats();
-        new LoaderLogManager().init();
+		new LoaderLogManager().init();
 		logger.info("bulk_boader_started");
 
 		config_.validation();
 		loadConfig();
 		ConfigurationService.getInstance();	
 		logger.info("configuation_loaded");
-	
+
 		if (isMasterLoader_) {
-		   logger.info("master_loader:" + loaderName_);
+			logger.info("master_loader:" + loaderName_);
 		} else {
-		   logger.info("slave_loader:" + loaderName_);
+			logger.info("slave_loader:" + loaderName_);
 		}
-		
-	    ObjectNodeUtil.initDataObjectAdapter();	
-	    dateFormatString_ = ObjectNodeUtil.getDateFormatString();
+
+		ObjectNodeUtil.initDataObjectAdapter();	
+		dateFormatString_ = ObjectNodeUtil.getDateFormatString();
 	}
-	
+
 	public void bulkMatchLoad() throws Exception {
-		
-	 if (loadMode_ != MIGENERATE_ONLY)	 {
-	  if (loadMode_ != BLOCKDISTRIBUTE_AFTER) {
-		clusterSynchronizer_.initLoaderName(loaderName_, isMasterLoader_);
-	    if (isMasterLoader_) { 
-		  
-		  clusterSynchronizer_.setClusterState(ClusterState.BLOCK_DISTRIBUTION);		
-	      BlockDistributor blockDistributor = new BlockDistributor(matchPaths_, inputLookup_, inputobd_, blockLk_, false);	
-	      RuntimeStats();
-	      logger.info("block_distribution_started");
-	      blockDistributor.distributeBlocks();
-	      logger.info("block_distribution_completed");
-	      RuntimeStats();
-	      
-	      clusterSynchronizer_.setClusterState(ClusterState.MATCHING);	    
-	    } else {
-		  logger.info("waiting_for_block_distribution");
-		  clusterSynchronizer_.waitMatchingReady();		 
-	    }
-	  }
-	    
-	    if (loadMode_ == BLOCKDISTRIBUTE_ONLY) {
-	    	return;
-	    }
-	    logger.info("matcher_started");	
-	    Matcher matcher = new Matcher(matchPaths_, matchTypes_, blockLk_, false, dateFormatString_);
-	    matcher.match();
-	    logger.info("matching_done");
-	    RuntimeStats();
-	 
-	    FileManager.deleteBlockDir(false);
-	    if (ismatchAnalyzer) {
-		  return;
-	    }
-	    logger.info("EUID_Assigner_started");	
-	  
-	    if (isMasterLoader_) { 
-	      EuidIndexAssigner euidAssigner = new EuidIndexAssigner();
-	      euidAssigner.start();
-	    }  else {
-		    clusterSynchronizer_.waitMasterIndexGenerationReady(); 
-	    }
-	    RuntimeStats();
-	  
-	    FileManager.deleteMatchDir(false);
-	  
-	    logger.info("EUID_Assigner_Done");
-	  	  	  
-	  }
-	 
-      if (loadMode_ != UPTO_EUIDASSIGN) {  // Do following operations only if mode is not EUIDASSIGN	 
-	    logger.info("master_index_generation_started");	
-	    MasterIndex masterIndex = new MasterIndex();
-	    masterIndex.generateMasterIndex();
-	    
-	    RuntimeStats();
-	    if (loadMode_ == MIGENERATE_ONLY ) {
-	      logger.info("Master Index Generated");
-	      
-	  	  System.exit(0);
-	    }
-	  
-	    File file = FileManager.getInputSBRFile();
-	    String f = file.getName();
-	  
-	    clusterSynchronizer_.setMasterIndexDone(f);
-	  
-	    if (isMasterLoader_) {
-		  clusterSynchronizer_.waitMasterIndexDone();
-	    }
-	  
-	    FileManager.deleteEUIDDir(false);
-	  
-	    logger.info("master_index_generation_completed");
-	  
-	    logger.info("potential_duplicates_started");
-	  
-	    if (isMasterLoader_) { 	 
-			clusterSynchronizer_.setClusterState(ClusterState.POT_DUPLICATE_BLOCK);							
-		    BlockDistributor blockDistributor = new BlockDistributor(sbrmatchPaths_, sbrLookup_, inputobd_,  sbrblockLk_, true);	
-		    blockDistributor.distributeBlocks();
-		    logger.info("Potental Dups SBR block distribution completed");
-		    clusterSynchronizer_.setClusterState(ClusterState.POT_DUPLICATE_MATCH);	    
-		 } else {
-			 //logger.info("waiting for Pot Dup SBR block distribution to be completed");
-			 clusterSynchronizer_.waitSBRMatchingReady();		 
-		 }
-		 
-		 	
-	   Matcher matcher = new Matcher(sbrmatchPaths_, matchTypes_, sbrblockLk_, true, dateFormatString_);
-	   matcher.match();
-	   logger.info("Potental Dups maching completed");
-	   FileManager.deleteSBRBlockDir(false);
-	   FileManager.deleteSBRInputDir(true);
-	   if (isMasterLoader_) { 	 
-	     PotDupGenerator potGen = new PotDupGenerator();
-	     potGen.generatePotDups();
-	   }
-	 
-	   logger.info("Potential duplicates completed");
-		   	  
-	   FileManager.deleteSBRMatchDir(false);
-	 
-	   if (bulkLoad) {	 
-	      BulkLoader bl = new BulkLoader();
-	      bl.load(); 
-	   }
-      }
-	 
-	  logger.info("Bulk Loader Completed");
-	
+
+		if (loadMode_ != MIGENERATE_ONLY)	 {
+			if (loadMode_ != BLOCKDISTRIBUTE_AFTER) {
+				clusterSynchronizer_.initLoaderName(loaderName_, isMasterLoader_);
+				if (isMasterLoader_) { 
+
+					clusterSynchronizer_.setClusterState(ClusterState.BLOCK_DISTRIBUTION);		
+					BlockDistributor blockDistributor = new BlockDistributor(matchPaths_, inputLookup_, inputobd_, blockLk_, false);	
+					RuntimeStats();
+					logger.info("block_distribution_started");
+					blockDistributor.distributeBlocks();
+					logger.info("block_distribution_completed");
+					RuntimeStats();
+
+					clusterSynchronizer_.setClusterState(ClusterState.MATCHING);	    
+				} else {
+					logger.info("waiting_for_block_distribution");
+					clusterSynchronizer_.waitMatchingReady();		 
+				}
+			}
+
+			if (loadMode_ == BLOCKDISTRIBUTE_ONLY) {
+				return;
+			}
+			logger.info("matcher_started");	
+			Matcher matcher = new Matcher(matchPaths_, matchTypes_, blockLk_, false, dateFormatString_);
+			matcher.match();
+			logger.info("matching_done");
+			RuntimeStats();
+
+			FileManager.deleteBlockDir(false);
+			if (ismatchAnalyzer) {
+				return;
+			}
+			logger.info("EUID_Assigner_started");	
+
+			if (isMasterLoader_) { 
+				EuidIndexAssigner euidAssigner = new EuidIndexAssigner();
+				euidAssigner.start();
+			}  else {
+				clusterSynchronizer_.waitMasterIndexGenerationReady(); 
+			}
+			RuntimeStats();
+
+			FileManager.deleteMatchDir(false);
+
+			logger.info("EUID_Assigner_Done");
+
+		}
+
+		if (loadMode_ != UPTO_EUIDASSIGN) {  // Do following operations only if mode is not EUIDASSIGN	 
+			logger.info("master_index_generation_started");	
+			MasterIndex masterIndex = new MasterIndex();
+			masterIndex.generateMasterIndex();
+
+			RuntimeStats();
+			if (loadMode_ == MIGENERATE_ONLY ) {
+				logger.info("Master Index Generated");
+
+				System.exit(0);
+			}
+
+			File file = FileManager.getInputSBRFile();
+			String f = file.getName();
+
+			clusterSynchronizer_.setMasterIndexDone(f);
+
+			if (isMasterLoader_) {
+				clusterSynchronizer_.waitMasterIndexDone();
+			}
+
+			FileManager.deleteEUIDDir(false);
+
+			logger.info("master_index_generation_completed");
+
+			logger.info("potential_duplicates_started");
+
+			if (isMasterLoader_) { 	 
+				clusterSynchronizer_.setClusterState(ClusterState.POT_DUPLICATE_BLOCK);							
+				BlockDistributor blockDistributor = new BlockDistributor(sbrmatchPaths_, sbrLookup_, inputobd_,  sbrblockLk_, true);	
+				blockDistributor.distributeBlocks();
+				logger.info("Potental Dups SBR block distribution completed");
+				clusterSynchronizer_.setClusterState(ClusterState.POT_DUPLICATE_MATCH);	    
+			} else {
+				//logger.info("waiting for Pot Dup SBR block distribution to be completed");
+				clusterSynchronizer_.waitSBRMatchingReady();		 
+			}
+
+
+			Matcher matcher = new Matcher(sbrmatchPaths_, matchTypes_, sbrblockLk_, true, dateFormatString_);
+			matcher.match();
+			logger.info("Potental Dups maching completed");
+			FileManager.deleteSBRBlockDir(false);
+			FileManager.deleteSBRInputDir(false);
+			if (isMasterLoader_) { 	 
+				PotDupGenerator potGen = new PotDupGenerator();
+				potGen.generatePotDups();
+			}
+
+			logger.info("Potential duplicates completed");
+
+			FileManager.deleteSBRMatchDir(false);
+
+			if (bulkLoad) {	 
+				BulkLoader bl = new BulkLoader();
+				bl.load(); 
+			}
+		}
+
+		logger.info("Bulk Loader Completed");
+
 	}
-	
+
 	private void cleanDirs() {
 		FileManager.deleteBlockDir(true);
 		FileManager.deleteMatchDir(true);
@@ -229,185 +229,184 @@ public class BulkMatcherLoader {
 		FileManager.deleteSBRMatchDir(true);
 		FileManager.deleteSBRInputDir(true);
 	}
-	
+
 	private void loadConfig() throws Exception {
-						
+
 		String workingDir = config_.getSystemProperty("workingDir");
 		loaderName_ = config_.getSystemProperty("loaderName");
 		String isMasterLoader = config_.getSystemProperty("isMasterLoader");
 		isMasterLoader_ = Boolean.parseBoolean(isMasterLoader);
-		
+
 		String isSmatchAnalyzer = config_.getSystemProperty("matchAnalyzerMode");
 		if (isSmatchAnalyzer != null) {
-		  ismatchAnalyzer = Boolean.parseBoolean(isSmatchAnalyzer);
+			ismatchAnalyzer = Boolean.parseBoolean(isSmatchAnalyzer);
 		}
-		
+
 		String sdelInterMediateDir = config_.getSystemProperty("deleteIntermediateDirs");
 		if (sdelInterMediateDir != null) {
 			delInterMediateDir = Boolean.parseBoolean(sdelInterMediateDir);
 		}
-		
+
 		String sbulkLoad = config_.getSystemProperty("BulkLoad");
 		if (sbulkLoad != null) {
 			bulkLoad = Boolean.parseBoolean(sbulkLoad);
 		}
-		
+
 		FileManager.setWorkingDir(workingDir, loaderName_, delInterMediateDir);
 		String sloadMode= config_.getSystemProperty("loadMode");
 		if (sloadMode != null) {
-			 if (sloadMode.equals("MIGENERATE_ONLY")) { 
-			  loadMode_ = MIGENERATE_ONLY;
+			if (sloadMode.equals("MIGENERATE_ONLY")) { 
+				loadMode_ = MIGENERATE_ONLY;
 			} else if (sloadMode.equals("UPTO_EUIDASSIGN")) { 
-			  loadMode_ = UPTO_EUIDASSIGN;
+				loadMode_ = UPTO_EUIDASSIGN;
 			} else if (sloadMode.equals("BLOCKDISTRIBUTE_AFTER")) { 
-			  loadMode_ = BLOCKDISTRIBUTE_AFTER;
+				loadMode_ = BLOCKDISTRIBUTE_AFTER;
 			} else if (sloadMode.equals("BLOCKDISTRIBUTE_ONLY")) { 
-			  loadMode_ = BLOCKDISTRIBUTE_ONLY;
+				loadMode_ = BLOCKDISTRIBUTE_ONLY;
 			} 
-        }
-		if (loadMode_ != MIGENERATE_ONLY && loadMode_ != BLOCKDISTRIBUTE_AFTER) {
-		  cleanDirs();
-		  FileManager.initDirs();
 		}
-				
-        clusterSynchronizer_ = ClusterSynchronizer.getInstance();
-		
-		
+		if (loadMode_ != MIGENERATE_ONLY && loadMode_ != BLOCKDISTRIBUTE_AFTER) {
+			cleanDirs();
+			FileManager.initDirs();
+		}
+
+		clusterSynchronizer_ = ClusterSynchronizer.getInstance();
+
 		List<String> matchlPaths = config_.getMatchFields();//readMatchConfiguration();	
 		String[] matchPaths = new String[matchlPaths.size()];
 		matchlPaths.toArray(matchPaths);
-		
+
 		matchTypes_ = readMatchTypes();
 		matchPaths_ = addExtraBucketFields(matchPaths);
 		sbrmatchPaths_ = addExtraSBRFields(matchPaths);				
 		initLookup();				
-				
+
 	}	
-	
-	
-	
-	
+
+
+
+
 	public static void  main(String[] args) {
 		try {		 
-		 BulkMatcherLoader bulkMatcher = new BulkMatcherLoader();
-		 bulkMatcher.bulkMatchLoad();
-		 
+			BulkMatcherLoader bulkMatcher = new BulkMatcherLoader();
+			bulkMatcher.bulkMatchLoad();
+
 		} catch (Throwable ex) {
 			logger.severe(ex + ex.getMessage());
 			logger.severe(Util.getStackTrace(ex));
 			ex.printStackTrace();
 		}			
 	}
-	
+
 	private void initLookup() throws Exception {
-				 		
+
 		ObjectDefinition obd = ObjectDefinitionBuilder.buildObjectDefinition(matchPaths_);
 		blockLk_ = Lookup.createLookup(obd);
-		
+
 		obd = ObjectDefinitionBuilder.buildObjectDefinition(sbrmatchPaths_);
 		sbrblockLk_ = Lookup.createLookup(obd);
-		
+
 		inputLookup_ = getInputLookup();
-		
+
 		sbrLookup_ = getSBRLookup();
 	}
-	
-		
+
+
 	private String[] readMatchTypes() {
 
 		List<String> matchlTypes = config_.getMatchFieldTypes();
 		String[] matchTypes = new String[matchlTypes.size()];
 		matchlTypes.toArray(matchTypes);
-		
+
 		return matchTypes;
 	}
-	
+
 	private String[] addExtraBucketFields(String[] paths) {
 		int size = paths.length + 4; 
 		String[] blockEpaths = new String[size];
 		int index = paths[0].indexOf(".");
-		
+
 		String primaryObject = null;
-		
+
 		if (index > 0) {
-		   primaryObject = paths[0].substring(0, index);	
+			primaryObject = paths[0].substring(0, index);	
 		}
-		
+
 		blockEpaths[0] = primaryObject + ".blockID";
 		blockEpaths[1] = primaryObject + ".GID";
 		blockEpaths[2] = primaryObject + ".systemcode";		
 		blockEpaths[3] = primaryObject + ".lid";
-		
+
 		System.arraycopy(paths, 0, blockEpaths, 4, paths.length);		
 		return blockEpaths;
 	}
-	
+
 	private String[] addExtraSBRFields(String[] paths) {
 		int size = paths.length + 2; 
 		String[] blockEpaths = new String[size];
 		int index = paths[0].indexOf(".");
-		
+
 		String primaryObject = null;		
 		if (index > 0) {
-		   primaryObject = paths[0].substring(0, index);	
+			primaryObject = paths[0].substring(0, index);	
 		}
-		
+
 		blockEpaths[0] = primaryObject + ".blockID";
 		blockEpaths[1] = primaryObject + ".EUID";
-			
+
 		System.arraycopy(paths, 0, blockEpaths, 2, paths.length);		
 		return blockEpaths;
 	}
-	
+
 	private Lookup getInputLookup() throws Exception {
 		ObjectDefinition obd = config_.getObjectDefinition();
 		Field f = new Field();
 		f.setName("GID");
 		obd.addField(0,f);
-		
-				
+
+
 		f = new Field();
 		f.setName("systemcode");
 		obd.addField(1,f);
-		
+
 		f = new Field();
 		f.setName("lid");
 		obd.addField(2,f);
-		
+
 		f = new Field();
 		f.setName("updateDateTime");
 		obd.addField(3,f);
-		
+
 		f = new Field();
 		f.setName("createUser");
 		obd.addField(4,f);
-		
+
 		inputobd_ = obd;
-	
+
 		Lookup lookup = Lookup.createLookup(obd);
 		return lookup;
 	}
-	
+
 	private Lookup getSBRLookup() throws Exception {
 		ObjectDefinition obd = config_.getObjectDefinition();
 		Field f = new Field();
 		f.setName("EUID");
 		obd.addField(0,f);
-		
+
 		Lookup lookup = Lookup.createLookup(obd);
 		return lookup;
 	}
-	
+
 	private void RuntimeStats() {
-		
+
 		Runtime rt = Runtime.getRuntime();
 		long free = rt.freeMemory();
 		long total = rt.totalMemory();
 		long max = rt.maxMemory();
-		
+
 		logger.info("Max Memory: " + max/1000000 + "M");
 		logger.info("total Memory: " + total/1000000 + "M");
 		logger.info("free Memory: " + free/1000000 + "M");
-		
+
 	}
 }
