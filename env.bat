41c95d1e890ee6ba26004ac2727814c68f61eb87
cye <cye@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-05-06 08:47:44 +0000

    added BlockDistributorTest Junit Tests

--- env.bat
+++ env.bat
@@ -5,6 +5,9 @@ set GF_HOME=C:\CAPS52\glassfish
 set MAVEN_HOME=C:\maven-2.0.7
 set RUN_JUNIT=no
 
+set OPEM_DM_MI_ROOT=%CD%
+set FULL_PATH_TO_LOADER_RESOURCES_JAR=%OPEM_DM_MI_ROOT%\loader\test\config\resources.jar
+set FULL_PATH_TO_LOADER_MASTER_INDEX_CLIENT_JAR=%OPEM_DM_MI_ROOT%\loader\test\config\master-index-client.jar
 
 set PATH=%MAVEN_HOME%/bin;%PATH%
 set ANT_OPTS=-Xmx512m
@@ -15,5 +18,7 @@ call mvn install:install-file -DgroupId=msft.sqlserver.jdbc -DartifactId=sqljdbc
 call mvn install:install-file -DgroupId=javaee -DartifactId=javaee-api -Dversion=5 -Dpackaging=jar -Dfile=%GF_HOME%/lib/javaee.jar
 call mvn install:install-file -DgroupId=org.netbeans.api -DartifactId=org-netbeans-modules-compapp-projects-base -Dversion=RELEASE60-BETA1 -Dpackaging=jar -Dfile=%NB_HOME%/soa2/modules/org-netbeans-modules-compapp-projects-base.jar
 
+call mvn install:install-file -DgroupId=loader.test -DartifactId=resources -Dversion=6.0 -Dpackaging=jar -Dfile=%FULL_PATH_TO_LOADER_RESOURCES_JAR%
+call mvn install:install-file -DgroupId=loader.test -DartifactId=master-index-client -Dversion=6.0 -Dpackaging=jar -Dfile=%FULL_PATH_TO_LOADER_MASTER_INDEX_CLIENT_JAR%
 
 
