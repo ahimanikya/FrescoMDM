8b0e24df9096b0ec0b5a0f2a9b9938ee2be54928
jbire <jbire@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-05-15 01:04:15 +0000

    sierra branch setup

--- tl/src/cmn/codeline/codeline.pl
+++ tl/src/cmn/codeline/codeline.pl
@@ -39,7 +39,7 @@ $TESTING = 0;
 #what is the release management codeline name?
 #warning - don't read this from the environment, as all
 #values in this file apply to a specific RE codeline.
-$CODELINE = "main";
+$CODELINE = "sierra";
 
 #official name of the product. Same comments as above:
 $PRODUCT = "odmmi";
@@ -57,12 +57,12 @@ $PRODUCT = "odmmi";
 );
 
 %MINOR_VERSION = (
-    'odmmi', '1',
+    'odmmi', '1.1',
 );
 
 #this is the current release number:
 %MILESTONE_VERSION = (
-    'odmmi', 'SM09',
+    'odmmi', 'SM10',
 );
 
 #this is the milestone date this release is based on.
