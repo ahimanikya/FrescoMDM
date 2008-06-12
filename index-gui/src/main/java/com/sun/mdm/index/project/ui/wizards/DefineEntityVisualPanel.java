0243d817aaa6bf689327b7fc6a9f7371ae5f16fb
weetan <weetan@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-06-12 17:23:13 +0000

    issue# 8: Netbeans config editor should warn user if field length for date is not large enough.  If the length for date data type is less than 10, the length will be default back to 10.

--- index-gui/src/main/java/com/sun/mdm/index/project/ui/wizards/DefineEntityVisualPanel.java
+++ index-gui/src/main/java/com/sun/mdm/index/project/ui/wizards/DefineEntityVisualPanel.java
@@ -857,12 +857,32 @@ public class DefineEntityVisualPanel extends javax.swing.JPanel
             if (!fieldNode.isField()) {
                 msg = "Primary object node \"" + primaryNode.getName() + "\" contains no fields!";
             } else {
+                boolean foundOneMatchType = false;
                 for (int i = 0; i < cnt; i++) {
                     EntityNode objNode = (EntityNode) primaryNode.getChildAt(i);
                     if (objNode.isSub() && objNode.getChildCount() <= 0) {
                         msg = "Object node \"" + objNode.getName() + "\" contains no fields!";
                         break;
                     }
+                    
+                    //if the node is sub node,check to make sure that sub node has at last one match type
+                    if (objNode.isSub()) {
+                        //for child sub node
+                        for (int j = 0; j < objNode.getChildCount(); j++) {
+                            EntityNode objChildNode = (EntityNode) objNode.getChildAt(j);
+                            if (objChildNode.getMatchType() != null) {
+                                foundOneMatchType = true;
+                            }
+                        }
+                    } else {
+                        if (objNode.getMatchType() != null) {
+                            foundOneMatchType = true;
+                        }
+                    }
+                }
+                
+                if (!foundOneMatchType) {
+                    msg = "The \"" + mRootNode.getName() + "\" Application must define at least one match type!";
                 }
             }
         } else {
