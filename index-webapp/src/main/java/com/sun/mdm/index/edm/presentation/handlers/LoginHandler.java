8b1e04ff22f591393a7895d992b5d1af1dafc402
pmalhotra <pmalhotra@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-07-02 08:44:58 +0000

    Incorporated changes to handle endless error messages to log in event of database unavailability.

--- index-webapp/src/main/java/com/sun/mdm/index/edm/presentation/handlers/LoginHandler.java
+++ index-webapp/src/main/java/com/sun/mdm/index/edm/presentation/handlers/LoginHandler.java
@@ -64,7 +64,7 @@ public class LoginHandler {
     private String password = "eview";
     private static final String SUCCESS ="success";
     private static final String FAILURE ="failure";
-    private static final String FAIL_INITIALIZATION = "initializationfailed";
+    public static final String FAIL_INITIALIZATION = "initializationfailed";
     private static final String SUCCESS_INITIALIZATION = "initializationsuccess";
     private static final String LOGOUT ="logout";
     private HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
@@ -89,23 +89,101 @@ public class LoginHandler {
     public void setPassword(String password) {
         this.password = password;
     }
+    /** 
+     * Added By Rajani Kanth
+     * 
+     * Modified on 03/06/2008
+     * This method is used to sign out of the application. This method will invalidate the session as well.
+     * 
+     * 
+     * @return String        
+     */
     
-    
-    public String authorizeAndLoginUser() {
-       
+    public String signOutUser() {
+        request.setAttribute("Logout", "LoggedOut");
+        session.invalidate();
+        return LOGOUT;
+    }
+    /** 
+     * Added By Rajani Kanth
+     * 
+     * Modified on 03/06/2008
+     * 
+     * This method is used to initialize the application parameteres
+     *
+     * @return String initializationfailed if Exception occurs      
+     */
+
+     public String  initializeApplication() {
+        int euidLength=10;
+        // This function is the entry point to EDM
+        // check if ConfigManager is available
+        try {
+            ConfigManager.init();
+        } catch (Exception t) {
+            request.setAttribute("Error while initializing ConfigManager", QwsUtil.getRootCause(t).getMessage());
+            mLogger.error(mLocalizer.x("HND008: Error while initializing ConfigManager {0}",t.getMessage()));
+            request.setAttribute(FAIL_INITIALIZATION, "Error while initializing ConfigManager : " + t.getMessage());
+            return FAIL_INITIALIZATION;
+        }
+
+        // check if MasterController is available
+        // To get to MasterController, ConfigManager is required.
+        if (QwsController.getMasterController() == null || QwsController.getValidationService() == null) {
+             try {
+                // this piece of info in ConfigManager comes from MC
+                ConfigManager.getInstance().setEuidLength(euidLength);
+            } catch (Exception e) {
+                 mLogger.error(mLocalizer.x("HND09: Error while initializing QwsController : {0}",e.getMessage()),e);
+                 request.setAttribute(FAIL_INITIALIZATION, "Error while initializing QwsController : " + QwsUtil.getRootCause(e).getMessage());
+                return FAIL_INITIALIZATION;
+            }
+ 
+             // initialize the date format in DateUtil for EDM to convert date/string back and forth
+            try {
+                DateUtil.init();
+            } catch (Exception t) {
+                mLogger.error(mLocalizer.x("HND010: Error while initializing DateUtil : {0}",t.getMessage()),t);
+               request.setAttribute(FAIL_INITIALIZATION, "Error while initializing DateUtil : " + QwsUtil.getRootCause(t).getMessage());
+               return FAIL_INITIALIZATION;
+            }
+        } else {
+            // Restart the ValidationService and DateUtil in case there were problems
+            // with logging in the EDM or if the database connection was lost.  Otherwise,
+            // the EDM will not display values for pull-down menus.
+            try {
+                ValidationService.init();
+            } catch (Exception t) {
+                mLogger.error(mLocalizer.x("HND011: Error while initializing ValidationService : {0}",t.getMessage()),t);
+                request.setAttribute(FAIL_INITIALIZATION, "Error while initializing ValidationService : " + QwsUtil.getRootCause(t).getMessage());
+               return FAIL_INITIALIZATION;
+            }
+            
+            // initialize the date format in DateUtil for EDM to convert date/string back and forth
+            try {
+                DateUtil.init();
+            } catch (Exception t) {
+                mLogger.error(mLocalizer.x("HND012: Error while initializing ValidationService : {0}",t.getMessage()),t);
+                request.setAttribute(FAIL_INITIALIZATION, "Error while initializing ValidationService : " + QwsUtil.getRootCause(t).getMessage());
+                return FAIL_INITIALIZATION;
+            }
+            
+        }
+        //authorizeAndLoginUser();
+        
         ResourceBundle bundle = ResourceBundle.getBundle(NavigationHandler.MIDM_PROP,FacesContext.getCurrentInstance().getViewRoot().getLocale());
         String errorMessage =bundle.getString("Login_Success");
         //Logon logon  = new Logon();
         //Use LogOn class here
         try {
-            Logon.initializeConfigurationSecurity();
-            
+            Logon.initializeConfigurationSecurity();            
         } catch(Exception ex) {
             
            // ex.printStackTrace();
             errorMessage = bundle.getString("login_user_login_init_load_message");
             FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,errorMessage,errorMessage));
             mLogger.error(mLocalizer.x("HND004: {0}:{1}",errorMessage,ex.getMessage()),ex);
+            request.setAttribute(FAIL_INITIALIZATION, errorMessage + " : " + QwsUtil.getRootCause(ex).getMessage());
             return FAIL_INITIALIZATION;
         }
         UserProfile userProfile = null;
@@ -121,11 +199,12 @@ public class LoginHandler {
             Logon.initializeQWSControllerValidationDate();
         } catch (Exception e) {
            // e.printStackTrace();
-            errorMessage = bundle.getString("login_user_login_failure_message");
+            errorMessage = bundle.getString("login_user_login_init_load_message");
             FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,errorMessage,errorMessage));
             mLogger.error(mLocalizer.x("HND005: {0} :{1}",errorMessage,e.getMessage()),e);
-            return FAILURE;
-
+            request.setAttribute(FAIL_INITIALIZATION, errorMessage + " : " + QwsUtil.getRootCause(e).getMessage());
+            return FAIL_INITIALIZATION;
+    
         }
         
         if( userProfile != null ) {
@@ -138,106 +217,20 @@ public class LoginHandler {
             } catch(Exception ex) {
                  //ex.printStackTrace();
                 mLogger.error(mLocalizer.x("HND006: {0} ",ex.getMessage()));
-                 return FAILURE;
+                request.setAttribute(FAIL_INITIALIZATION, "Failed to get the initial screen id : " + QwsUtil.getRootCause(ex).getMessage());
+                return FAIL_INITIALIZATION;
             }
             return initialScreenName;            
         } else {
             errorMessage = bundle.getString("login_user_login_failure_message");
             FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,errorMessage,errorMessage));
             mLogger.error(mLocalizer.x("HND007: {0} :{1}",errorMessage));
-            return FAILURE;
-        } 
-    }
-    
-    public String signOutUser() {
-        request.setAttribute("Logout", "LoggedOut");
-        session.invalidate();
-        return LOGOUT;
-    }
+            request.setAttribute(FAIL_INITIALIZATION, "Failed to create the user profile.");
 
-    
-    public String  initializeApplication() {
-        int euidLength=10;
-        // This function is the entry point to EDM
-        // check if ConfigManager is available
-        try {
-            ConfigManager.init();
-        } catch (Exception t) {
-            request.setAttribute("Error while initializing ConfigManager", QwsUtil.getRootCause(t).getMessage());
-            mLogger.error(mLocalizer.x("HND008: Error while initializing ConfigManager {0}",t.getMessage()));
             return FAIL_INITIALIZATION;
-        }
+        } 
 
-        // check if MasterController is available
-        // To get to MasterController, ConfigManager is required.
-        if (QwsController.getMasterController() == null || QwsController.getValidationService() == null) {
-           /*            
-            try {
-                QwsController.init();
-            } catch (Exception t) {
-                request.setAttribute("Error while initializing QwsController", QwsUtil.getRootCause(t).getMessage());
-                return FAIL_INITIALIZATION;
-            }
-            int euidLength = 32;
-            try {
-                euidLength = ((Integer) QwsController.getMasterController().getConfigurationValue("EUID_LENGTH")).intValue();
-            } catch (Exception e) {
-                // UserException doesn't need a stack trace, and ProcessingException
-                // stack trace is already logged in the MC.
-                request.setAttribute("Error while initializing QwsController", QwsUtil.getRootCause(e).getMessage());
-                
-            }
-            */
-            try {
-                // this piece of info in ConfigManager comes from MC
-                ConfigManager.getInstance().setEuidLength(euidLength);
-            } catch (Exception e) {
-                 mLogger.error(mLocalizer.x("HND09: Error while initializing QwsController : {0}",e.getMessage()),e);
-                request.setAttribute("Error while initializing QwsController", QwsUtil.getRootCause(e).getMessage());
-            }
- 
-            /*
-            // re-init ValidationService as it depends on CodeLookupService and MasterController
-            try {
-                ValidationService.init();
-            } catch (Exception t) {
-                request.setAttribute("Error while initializing ValidationService", QwsUtil.getRootCause(t).getMessage());
-                return FAIL_INITIALIZATION;
-            }
-            */
-            // initialize the date format in DateUtil for EDM to convert date/string back and forth
-            try {
-                DateUtil.init();
-            } catch (Exception t) {
-                request.setAttribute("Error while initializing DateUtil", QwsUtil.getRootCause(t).getMessage());
-               mLogger.error(mLocalizer.x("HND010: Error while initializing DateUtil : {0}",t.getMessage()),t);
-                return FAIL_INITIALIZATION;
-            }
-        } else {
-            // Restart the ValidationService and DateUtil in case there were problems
-            // with logging in the EDM or if the database connection was lost.  Otherwise,
-            // the EDM will not display values for pull-down menus.
-            try {
-                ValidationService.init();
-            } catch (Exception t) {
-                request.setAttribute("Error while initializing ValidationService", QwsUtil.getRootCause(t).getMessage());
-                 mLogger.error(mLocalizer.x("HND011: Error while initializing ValidationService : {0}",t.getMessage()),t);
-                return FAIL_INITIALIZATION;
-            }
-            
-            // initialize the date format in DateUtil for EDM to convert date/string back and forth
-            try {
-                DateUtil.init();
-            } catch (Exception t) {
-                request.setAttribute("Error while initializing ValidationService", QwsUtil.getRootCause(t).getMessage());
-                mLogger.error(mLocalizer.x("HND012: Error while initializing ValidationService : {0}",t.getMessage()),t);
-                return FAIL_INITIALIZATION;
-            }
-            
-        }
-        authorizeAndLoginUser();
-        return SUCCESS_INITIALIZATION;
-    }
+      }
     
     
     
