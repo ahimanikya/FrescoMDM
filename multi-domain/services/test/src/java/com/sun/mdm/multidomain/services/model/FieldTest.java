fc94241c8fe2f76e7f4de79ed13dfe6350a89450
cye <cye@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-11-06 05:37:56 +0000

    Refactored multi-domain services and added Junit tests.

---
+++ multi-domain/services/test/src/java/com/sun/mdm/multidomain/services/model/FieldTest.java
@@ -0,0 +1,45 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2003-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the terms of the Common 
+ * Development and Distribution License ("CDDL")(the "License"). You 
+ * may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://open-dm-mi.dev.java.net/cddl.html
+ * or open-dm-mi/bootstrap/legal/license.txt. See the License for the 
+ * specific language governing permissions and limitations under the  
+ * License.  
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice 
+ * in each file and include the License file at
+ * open-dm-mi/bootstrap/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the 
+ * fields enclosed by brackets [] replaced by your own identifying 
+ * information: "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package com.sun.mdm.multidomain.services.model;
+
+import junit.framework.TestCase;
+
+/**
+ * FieldTest class.
+ * @author cye
+ */
+public class FieldTest extends TestCase {
+
+    public FieldTest (String name) {
+        super(name);
+    }
+    
+    public void setUp() {
+    }
+    
+    public void test001() {
+        Field f = new Field("FOO", "FOO");
+        assertTrue("FOO".equals(f.getName()));
+        assertTrue("FOO".equals(f.getValue()));
+    }
+}
