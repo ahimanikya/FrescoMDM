ad5557d31043303d703b20fb216bbec66350487e
cye <cye@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-11-06 23:27:31 +0000

    Implemented multidomain services.

---
+++ multi-domain/services/test/src/java/com/sun/mdm/multidomain/services/model/RelationshipSearchTest.java
@@ -0,0 +1,50 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2003-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the terms of the Common 
+ * Development and Distribution License ("CDDL")(the "License"). You 
+ * may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://open-dm-mi.dev.java.net/cddl.html
+ * or open-dm-mi/bootstrap/legal/license.txt. See the License for the 
+ * specific language governing permissions and limitations under the  
+ * License.  
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice 
+ * in each file and include the License file at
+ * open-dm-mi/bootstrap/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the 
+ * fields enclosed by brackets [] replaced by your own identifying 
+ * information: "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package com.sun.mdm.multidomain.services.model;
+
+import com.sun.mdm.multidomain.services.relationship.RelationshipSearch;
+import java.util.Date;
+import junit.framework.TestCase;
+
+/**
+ * RelationshipSearchTest class.
+ * @author cye
+ */
+public class RelationshipSearchTest extends TestCase {
+
+    public RelationshipSearchTest (String name) {
+        super(name);
+    }
+    
+    public void setUp() {
+    }
+    
+    public void test001() {
+        RelationshipSearch rs = new RelationshipSearch();
+        rs.setName("FOO");
+        Date d = new Date();
+        rs.setPurgeDate(d);
+        assertTrue("FOO".equals(rs.getName()));
+        assertTrue(d.equals(rs.getPurgeDate()));
+    }
+}
---
+++ multi-domain/services/test/src/java/com/sun/mdm/multidomain/services/relationship/RelationshipDefinitionTest.java
@@ -0,0 +1,56 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2003-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the terms of the Common 
+ * Development and Distribution License ("CDDL")(the "License"). You 
+ * may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://open-dm-mi.dev.java.net/cddl.html
+ * or open-dm-mi/bootstrap/legal/license.txt. See the License for the 
+ * specific language governing permissions and limitations under the  
+ * License.  
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice 
+ * in each file and include the License file at
+ * open-dm-mi/bootstrap/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the 
+ * fields enclosed by brackets [] replaced by your own identifying 
+ * information: "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package com.sun.mdm.multidomain.services.relationship;
+
+import com.sun.mdm.multidomain.services.model.AttributeDef;
+
+import junit.framework.TestCase;
+
+/**
+ * RelationshipDefinitionTest class.
+ * @author cye
+ */
+public class RelationshipDefinitionTest extends TestCase {
+
+    public RelationshipDefinitionTest (String name) {
+        super(name);
+    }
+    
+    public void setUp() {
+    }
+    
+    public void test001() {
+        RelationshipDefinition rDef = new RelationshipDefinition();
+        rDef.setName("FOO");
+        assertTrue("FOO".equals(rDef.getName()));        
+    }
+
+    public void test002() {
+        RelationshipDefinition rDef = new RelationshipDefinition();
+        AttributeDef aDef = new AttributeDef();
+        aDef.setName("FOO");
+        rDef.setExtendedAttribute(aDef);
+        assertTrue("FOO".equals(rDef.getExtendeddAttribute("FOO").getName()));
+    }
+    
+}
