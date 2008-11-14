6ab9124c9447a2fec5dd071cf8a223bfd3d7a09d
cye <cye@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-11-14 19:33:22 +0000

    added HierarchyDef handling.

--- multi-domain/webapp/src/java/com/sun/mdm/multidomain/presentation/beans/HierarchyDefHandler.java
+++ multi-domain/webapp/src/java/com/sun/mdm/multidomain/presentation/beans/HierarchyDefHandler.java
@@ -25,7 +25,7 @@ package com.sun.mdm.multidomain.presentation.beans;
 import  java.util.List;
 import  java.util.ArrayList;
         
-import com.sun.mdm.multidomain.hierarchy.HierarchyDef;
+import com.sun.mdm.multidomain.services.hierarchy.HierarchyDefExt;
 
 import com.sun.mdm.multidomain.services.control.HierarchyManager;
 import com.sun.mdm.multidomain.services.core.ServiceManagerFactory;
@@ -45,17 +45,120 @@ public class HierarchyDefHandler {
         throws ServiceException { 
         hierarchyManager = ServiceManagerFactory.Instance().createHierarchyManager();
     }
-    public List<HierarchyDef> getHierarchyDefs(String domain) 
+    
+   /**
+     * Get all HirarchyDefs for the given domain name.
+     * @param domain Domain name.
+     * @return List<HierarchyDefEx> List of HierarchyDefEx.
+     * @exception ServiceException Thrown if an error occurs during processing. 
+     */    
+    public List<HierarchyDefExt> getHierarchyDefs(String domain) 
         throws ServiceException { 
-        List<HierarchyDef> types = null;
+        List<HierarchyDefExt> types = null;
         try {
             types = hierarchyManager.getHierarchyDefs(domain);
         } catch(ServiceException sex) {
             throw sex;
         }
         if(types == null) {
-            types = new ArrayList<HierarchyDef>();
+            types = new ArrayList<HierarchyDefExt>();
         }
         return types;        
    }
+    
+    /**
+     * Get HierarchyDef definition for the given hierarchy name and domain name.
+     * @param name HierarchyDef name.
+     * @return HierarchyDefExt HierarchyDefExt.
+     * @throws ServiceException Thrown if an error occurs during processing.
+     */
+    public HierarchyDefExt getHierarchyDefByName(String name, String domain) 
+        throws ServiceException {                
+        HierarchyDefExt type = null;
+        try {
+            type = hierarchyManager.getHierarchyDefByName(name, domain);
+        } catch(ServiceException svcex) {
+            throw svcex;
+        }
+        return type;
+    }
+    
+     /**
+     * Get HierarchyDef definition for the given hierarchy Id.
+     * @param hierarchyId HierarchyId.
+     * @return HierarchyDefExt HierarchyDefExt.
+     * @throws ServiceException Thrown if an error occurs during processing.
+     */
+    public HierarchyDefExt getHierarchyDefById(long hierarchyId) 
+        throws ServiceException {                
+        HierarchyDefExt type = null;
+        try {
+            type = hierarchyManager.getHierarchyDefById(hierarchyId);
+        } catch(ServiceException svcex) {
+            throw svcex;
+        }
+        return type;
+    }
+    
+    /**
+     * Create a new hierarchy definition.
+     * @param hDefExt HierarchyDefExt.
+     * @return String Hierarchy Identifier which is newly added.
+     * @exception ServiceException Thrown if an error occurs during processing. 
+     */
+    public String addHierarchyDef(HierarchyDefExt hDefExt) 
+        throws ServiceException {        
+        String HierarchyDefId = null;
+        try {
+            HierarchyDefId = hierarchyManager.addHierarchyDef(hDefExt);
+        } catch(ServiceException svcex) {
+            throw svcex;
+        }
+        return HierarchyDefId;        
+    }
+    
+    /**
+     * Update an existing hierarchy definition.
+     * @param hDefExt HierarchyDefExt.
+     * @exception ServiceException Thrown if an error occurs during processing. 
+     */
+    public void updateHierarchyDef(HierarchyDefExt hDefExt) 
+        throws ServiceException {        
+        try {
+            hierarchyManager.updateHierarchyDef(hDefExt);
+        } catch(ServiceException svcex) {
+            throw svcex;
+        }    
+    }
+    
+    /**
+     * Delete an existing HierarchyDef.
+     * @param hDefExt HierarchyDefExt.
+     * @exception ServiceException Thrown if an error occurs during processing. 
+     */
+    public void deleteHierarchyDef(HierarchyDefExt hDefExt) 
+        throws ServiceException {        
+        try {
+            hierarchyManager.deleteHierarchyDef(hDefExt);
+        } catch(ServiceException svcex) {
+            throw svcex;
+        }        
+    }
+    
+    /**
+     * Get total number of HierarchyDef for the given domain.
+     * @param domain Domain name.
+     * @return int Number of total HierarchyDefs.
+     * @throws ServiceException Thrown if an error occurs during processing.
+     */
+    public int getHierarchyDefCount(String domain)
+        throws ServiceException { 
+        int count = 0;
+        try {
+            count = hierarchyManager.getHierarchyDefCount(domain);
+        } catch(ServiceException svcex) {
+            throw svcex;
+        }   
+        return count;
+    }   
 }
