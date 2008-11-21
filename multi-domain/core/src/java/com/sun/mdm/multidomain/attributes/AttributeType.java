41882b3227b4c42388d0ad183ca0a77d4d1f34ec
davidpeh <davidpeh@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-11-21 01:48:11 +0000

    Initial check-in of CRUD Api

--- multi-domain/core/src/java/com/sun/mdm/multidomain/attributes/AttributeType.java
+++ multi-domain/core/src/java/com/sun/mdm/multidomain/attributes/AttributeType.java
@@ -23,110 +23,39 @@
 package com.sun.mdm.multidomain.attributes;
 
 /**
- *  AttributeType class.
- * @author cye 
+ *
+ * @author davidp
  */
-public class AttributeType {
-    public static final int UNKNOWN = -1;
-    /** Integer */
-    public static final int INTEGER = 0;
-    /** Boolean */
-    public static final int BOOLEAN = 1;
-    /** String */
-    public static final int STRING = 2;
-    /** Byte */
-    public static final int BYTE = 3;
-    /** Long */
-    public static final int LONG = 4;
-    /** Raw */
-    public static final int RAW = 5;
-    /** Float */
-    public static final int FLOAT = 7;
-    /** Date */
-    public static final int DATE = 6;
-    /** Timestamp */
-    public static final int TIMESTAMP = 8;
-    /** Character */
-    public static final int CHAR = 9;
-    /** Max number of attribute types */
-    static final int MAX_TYPES = 10;
+public enum AttributeType {
 
-    static final String[] DESCRIPTION = {"unknown", "Integer", "Boolean", "String", "Byte",
-                          "Long", "Blob", "Float", "Date", "Timestamp", "Character"};
-            
-    private int type = UNKNOWN;
-            
-    /**
-     * Create an instance of AttributeType.
-     */
-    public AttributeType() {
+    BOOLEAN("Boolean"),
+    CHAR("Character"),
+    DATE("Date"),
+    FLOAT("Float"),
+    INTEGER("Integer"),
+    STRING("String"),;
+    /*
+     * Attribute type description
+     * */
+    public final String description;
+
+    /* Defines attribute types with descriptions.
+     * */
+    AttributeType(String desc) {
+        this.description = desc;
     }
 
     /**
-     * Create an instance of AttributeType.
-     * @param type Attribute type.
+     * Method 'types'
+     *
+     * @return List A list of available attribute types.
      */
-    public AttributeType(String type) {
-        for (int i = 0; i < DESCRIPTION.length; i++) {            
-            if (DESCRIPTION[i].equalsIgnoreCase(type)) {                
-                this.type = i;
-            }            
-        }
+    public static AttributeType[] types() {
+        return AttributeType.values();
     }
-    
-    /**
-     * Create an instance of AttributeType.
-     * @param type Attribute type.
-     */
-    public AttributeType(int type) {
-        this.type = type;
-    }
-    
-    /**
-     * Set type.
-     * @param type Type.
-     */
-    public void setType(int type) {
-        this.type = type;
-    }
-    
-    /**
-     * Get type.
-     * @return int Type.
-     */
-    public int getType() {
-        return type;
-    }
-    
-    /**
-     * Get description in string.
-     * @return String Description.
-     */
-    public String getDescription() {
-        if (type > -1 &&
-            type < 10) {
-            return DESCRIPTION[type + 1];    
-        } else {
-            return DESCRIPTION[0];
-        }
-    }
-    
-    /**
-     * @see java.lang.Object#equals().
-     */
+
     @Override
-    public boolean equals(Object that) {
-        if(this == that) {
-            return true;
-        } else if (that == null) {
-            return false;
-        }
-        AttributeType thatType = (AttributeType)that;
-        if (thatType != null &&
-            this.getType() == thatType.getType()) {
-            return true;
-        } else {
-            return false;
-        }
+    public String toString() {
+        return description;
     }
 }
