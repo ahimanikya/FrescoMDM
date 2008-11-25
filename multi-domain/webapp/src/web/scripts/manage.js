2a3bd7f6e4d6e51e0faf427b12e087cdeb4562df
harishbn <harishbn@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-11-25 06:27:52 +0000

    showRecordFullDetails method added

--- multi-domain/webapp/src/web/scripts/manage.js
+++ multi-domain/webapp/src/web/scripts/manage.js
@@ -1,2 +1,13 @@
 
 // Manage screen related scripts
+
+ function showRecordFullDetails(contentId, showDetailsFlag){
+     if(! showDetailsFlag){
+         document.getElementById(contentId).style.visibility='hidden';
+        document.getElementById(contentId).style.display='none';
+     }
+     else{
+        document.getElementById(contentId).style.visibility='visible';
+        document.getElementById(contentId).style.display='block';
+     }
+ }
