5bcbb331eb1e0b73693a184e49cb5b2e5d6a8c6e
harishbn <harishbn@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-11-27 08:54:10 +0000

    GenericTitlePane added, changes made to css & RecordDetailsTitlePane

--- multi-domain/webapp/src/web/scripts/dijit/GenericTitlePane.js
+++ multi-domain/webapp/src/web/scripts/dijit/GenericTitlePane.js
@@ -1,5 +1,5 @@
-dojo._hasResource["dijit.RecordDetailsTitlePane"]=true;
-dojo.provide("dijit.RecordDetailsTitlePane");
+dojo._hasResource["dijit.GenericTitlePane"]=true;
+dojo.provide("dijit.GenericTitlePane");
 
 var summaryOn = new Image();
 summaryOn.src="images/icons/summary-button-down.gif";
@@ -10,15 +10,14 @@ detailsOn.src="images/icons/details-button-down.gif";
 var detailsOff=new Image();
 detailsOff.src="images/icons/details-button.gif";
            
-dojo.declare("dijit.RecordDetailsTitlePane", dijit.TitlePane, {
+dojo.declare("dijit.GenericTitlePane", dijit.TitlePane, {
     detailsShownFlag : false,
     
     templateString: '<div class="dijitTitlePane">' +
             '    <div class="CustomTitlePaneTitleBar" dojoAttachPoint="titleBarNode" style="cursor: default;height:22px;">' +
             '        <div dojoAttachEvent="onclick:toggle,onkeypress: _onTitleKey,onfocus:_handleFocus,onblur:_handleFocus" tabindex="0"' +
             '                waiRole="button" dojoAttachPoint="focusNode,arrowNode" class="dijitInline dijitArrowNode" style="cursor: pointer;float:left;"><span dojoAttachPoint="arrowNodeInner" class="dijitArrowNodeInner"></span></div>' +
-            '        <img src="images/icons/document_icon.png" border="0" align="left" hspace="2"><span dojoAttachPoint="titleNode" class="dijitTitlePaneTextNode" style="float:left"></span>'+
-            '       <span style="float:right;" class="dijitTitlePaneTextNode"><a href="javascript:void(0);"  dojoAttachEvent="onclick:toggleSummaryIcon"><img dojoAttachPoint="summaryIconNode" id="summaryIcon" src="images/icons/summary-button-down.gif" border="0" title="Summary" alt="Summary"></a>&nbsp;<a href="javascript:void(0);" dojoAttachEvent="onclick:toggleDetailsIcon"><img dojoAttachPoint="detailsIconNode" id="detailsIcon" src="images/icons/details-button.gif" border="0" title="Details" alt="Details" ></a></span>' +
+            '        <span dojoAttachPoint="titleNode" class="dijitTitlePaneTextNode" style="float:left"></span>'+
             '    </div>' +
             '    <div class="dijitTitlePaneContentOuter" dojoAttachPoint="hideNode">' +
             '        <div class="dijitReset" dojoAttachPoint="wipeNode">' +
