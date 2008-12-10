90cf4c7e38ed766976e495fed92ad627eda49467
davidpeh <davidpeh@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-12-10 01:16:01 +0000

    Added Domains database table

---
+++ multi-domain/core/src/java/com/sun/mdm/multidomain/relationship/service/DomainService.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sun.mdm.multidomain.relationship.service;
+
+import com.sun.mdm.multidomain.relationship.ops.exceptions.DaoException;
+import java.sql.Connection;
+import com.sun.mdm.multidomain.relationship.Domain;
+import com.sun.mdm.multidomain.relationship.ops.impl.DomainDaoImpl;
+
+/**
+ *
+ * @author davidp
+ */
+public class DomainService {
+
+    private Connection mConn = null;
+
+    /**
+     * Method 'DomainService'
+     *
+     */
+    public DomainService() {
+    }
+
+    /**
+     * Method 'DomainService'
+     *
+     */
+    public DomainService(Connection conn) {
+        this.mConn = conn;
+    }
+
+    public void addDomain(Domain domn) throws DaoException {
+        new DomainDaoImpl(mConn).insert(domn);
+    }
+
+    public Domain[] getDomains() throws DaoException {
+        return new DomainDaoImpl(mConn).getDomains();
+    }
+}
