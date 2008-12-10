6b4149079d7447c8977a08e16c198cf7b48c47e7
davidpeh <davidpeh@00cabb6a-c93c-0410-93ac-b9195fe8392e>
2008-12-10 19:55:09 +0000

    Modified the constructor signature

--- multi-domain/core/src/java/com/sun/mdm/multidomain/sql/BaseLogicCriteria.java
+++ multi-domain/core/src/java/com/sun/mdm/multidomain/sql/BaseLogicCriteria.java
@@ -4,9 +4,6 @@
  */
 package com.sun.mdm.multidomain.sql;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  *
  * @author davidp
@@ -14,15 +11,7 @@ import java.util.List;
 public abstract class BaseLogicCriteria extends Criteria {
 
     private String operator;
-    private Criteria left;
-    private Criteria right;
     private Criteria[] expressions = null;
-
-    public BaseLogicCriteria(String operator, Criteria left, Criteria right) {
-        this.left = left;
-        this.right = right;
-        this.operator = operator;
-    }
 
     public BaseLogicCriteria(String operator, Criteria... expressions) {
         this.expressions = expressions;
@@ -31,7 +20,7 @@ public abstract class BaseLogicCriteria extends Criteria {
 
     public String write() {
         StringBuffer sb = new StringBuffer();
-        sb.append("("); 
+        sb.append("(");
         for (int i = 0; i < expressions.length; i++) {
             sb.append(expressions[i].write());
             if (i < expressions.length - 1) {
@@ -41,18 +30,6 @@ public abstract class BaseLogicCriteria extends Criteria {
             }
         }
         sb.append(")");
-        return sb.toString();
-    }
-
-    public String write(String x) {
-        StringBuffer sb = new StringBuffer();
-        sb.append("( ");
-        sb.append(left.write());
-        sb.append(' ');
-        sb.append(operator);
-        sb.append(' ');
-        sb.append(right.write());
-        sb.append(" )");
         return sb.toString();
     }
 }
